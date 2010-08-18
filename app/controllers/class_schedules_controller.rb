106bd3e7609d1c388a9e3e7800f416c6d7b44b74
elmongui <elmongui@gmail.com>
2010-08-18 05:50:03 -0400

    remove id from create/update activescaffold

--- app/controllers/class_schedules_controller.rb
+++ app/controllers/class_schedules_controller.rb
@@ -1,6 +1,8 @@
 class ClassSchedulesController < ApplicationController
 	active_scaffold :class_schedule do |config|
 		config.columns = [:id, :course, :au_crn, :semester, :year, :day, :au_time_from, :au_time_to, :location, :vtel, :offered, :instructors, :registered_classes]
+		create.columns.exclude :id
+		update.columns.exclude :id
 		config.list.sorting = [{:year => :asc}, {:semester => :asc}]
 	end
 end
--- app/controllers/registered_classes_controller.rb
+++ app/controllers/registered_classes_controller.rb
@@ -1,5 +1,7 @@
 class RegisteredClassesController < ApplicationController
 	active_scaffold :registered_class do |config|
 		config.columns = [:id, :class_schedule, :student, :grade]
+		create.columns.exclude :id
+		update.columns.exclude :id
 	end
 end
