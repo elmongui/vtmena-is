a6601526231ea711ad368d5930d4ee2696ca1cb6
elmongui <elmongui@gmail.com>
2010-08-18 14:06:05 -0400

    class_schedule controller to remove id, registered_classes

--- app/controllers/class_schedules_controller.rb
+++ app/controllers/class_schedules_controller.rb
@@ -1,8 +1,6 @@
 class ClassSchedulesController < ApplicationController
 	active_scaffold :class_schedule do |config|
-		config.columns = [:id, :course, :au_crn, :semester, :year, :day, :au_time_from, :au_time_to, :location, :vtel, :offered, :instructors, :registered_classes]
-		create.columns.exclude :id
-		update.columns.exclude :id
+		config.columns = [:course, :au_crn, :semester, :year, :day, :au_time_from, :au_time_to, :location, :vtel, :offered, :instructors]
 		config.list.sorting = [{:year => :asc}, {:semester => :asc}]
 	end
 end
