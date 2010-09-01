2240ae5e660fdc63b53cb5d070cbe21a314e0031
elmongui <elmongui@gmail.com>
2010-09-01 11:00:20 -0400

    protect_from_forgery info_manager

--- app/controllers/info_manager_controller.rb
+++ app/controllers/info_manager_controller.rb
@@ -1,5 +1,6 @@
 class InfoManagerController < ApplicationController
 	before_filter :require_auth, :except => 'login'
+	protect_from_forgery :only => [:create, :update, :destroy]
 
 	def login
 		if request.post?
