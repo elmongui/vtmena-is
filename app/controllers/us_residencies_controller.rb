5b25719979a812cb4572aa77eb40fb3720f1ffe9
elmongui <elmongui@gmail.com>
2010-09-01 08:40:57 -0400

    adding person to us_residencies controller

--- app/controllers/us_residencies_controller.rb
+++ app/controllers/us_residencies_controller.rb
@@ -1,6 +1,6 @@
 class UsResidenciesController < ApplicationController
   active_scaffold :us_residency do |config|
-    config.columns = [:date_started, :date_ended, :address_1, :address_2, :city, :state, :zipcode, :phones]
+    config.columns = [:person, :date_started, :date_ended, :address_1, :address_2, :city, :state, :zipcode, :phones]
     create.columns.exclude :person
 	show.columns.exclude :person
 	update.columns.exclude :person
