fe164c0dce103e2b9bd89aa5d18b8706d6299898
elmongui <elmongui@gmail.com>
2010-09-01 08:42:27 -0400

    removing phones from us_controller

--- app/controllers/us_residencies_controller.rb
+++ app/controllers/us_residencies_controller.rb
@@ -1,8 +1,8 @@
 class UsResidenciesController < ApplicationController
   active_scaffold :us_residency do |config|
     config.columns = [:person, :date_started, :date_ended, :address_1, :address_2, :city, :state, :zipcode, :phones]
-    create.columns.exclude :person
-	show.columns.exclude :person
-	update.columns.exclude :person
+    create.columns.exclude :person, :phones
+	show.columns.exclude :person, :phones
+	update.columns.exclude :person, :phones
   end
 end
