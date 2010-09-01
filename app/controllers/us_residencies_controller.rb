a8d015739d2278eb21df161c8ec9fa32836cfeeb
elmongui <elmongui@gmail.com>
2010-09-01 08:46:43 -0400

    debugging

--- app/controllers/us_residencies_controller.rb
+++ app/controllers/us_residencies_controller.rb
@@ -1,8 +1,10 @@
 class UsResidenciesController < ApplicationController
   active_scaffold :us_residency do |config|
-    config.columns = [:person, :date_started, :date_ended, :address_1, :address_2, :city, :state, :zipcode, :phones]
+    config.columns = [:person_id, :date_started, :date_ended, :address_1, :address_2, :city, :state, :zipcode]
+=begin
     create.columns.exclude :person, :phones
 	show.columns.exclude :person, :phones
 	update.columns.exclude :person, :phones
+=end
   end
 end
